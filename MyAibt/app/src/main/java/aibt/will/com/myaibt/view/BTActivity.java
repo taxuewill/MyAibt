9ca589226d3ef7c1594ca790b199e38f53926a38
jing.wang <jing.wang@carrobot.com>
2017-06-21 11:10:13 +0800

    test

--- MyAibt/app/src/main/java/aibt/will/com/myaibt/view/BTActivity.java
+++ MyAibt/app/src/main/java/aibt/will/com/myaibt/view/BTActivity.java
@@ -32,6 +32,7 @@ public class BTActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bt);
+
         init();
     }
 
